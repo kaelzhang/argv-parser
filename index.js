e7d17e81ec65e60e2e65828d9f07b684461052cd
kaelzhang <i@kael.me>
2013-10-09 22:36:30 +0800

    refractor TYPE enum; update test case

--- index.js
+++ index.js
@@ -1,11 +1,156 @@
 'use strict';
 
-var nopt = require('nopt');
-var parser = module.exports = {};
+var parser      = module.exports = {};
+
+// var nopt        = require('nopt');
+var checker     = require('checker');
+var node_path   = require('path');
+var node_url    = require('url');
+var node_stream = require('stream');
+
 
 //  0       1           2          3
 // ['node', __filename, <command>, [options] ]
 parser.PARSE_ARGV_OFFSET = 2;
+
+
+// Part of the built-in enum types from nopt 
+
+// Copyright 2009, 2010, 2011 Isaac Z. Schlueter.
+// All rights reserved.
+
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+var TYPES = {
+    string: {
+        type: String,
+        setter: function (value) {
+            // If is normal string, strip html tags to prevent XSS attack
+            return String(value).replace(/<[^>]+>/g, '');
+        }
+    },
+
+    number: {
+        type: Number,
+        validator: function (value) {
+            return !isNaN(value);
+        },
+        setter: function (value) {
+            return Number(value);
+        }
+    },
+
+    boolean: {
+        type: Boolean,
+        setter: function (value) {
+            if ( value instanceof Boolean ) {
+                return value.valueOf();
+
+            } else if ( typeof value === 'string' ) {
+                if ( !isNaN(value) ) {
+                    return !!(+value);
+
+                } else if ( value === 'null' || value === 'false' ) {
+                    return false;
+
+                } else {
+                    return true;
+                }
+            
+            } else {
+                return !!value;
+            }
+        }
+    },
+
+    date: {
+        type: Date,
+        setter: function (value) {
+            var done = this.async();
+
+            var date = Date.parse(value);
+
+            if (isNaN(date)){
+                done({
+                    code: 'ETYPE',
+                    message: '"' + value + '" is not a valid date.',
+                    data: {
+                        value: value
+                    }
+                });
+            } else {
+                done(null, new Date(value));
+            }
+        }
+    },
+
+    html: {
+        type: 'html',
+        setter: function (value) {
+            return String(value);
+        }
+    },
+
+    url: {
+        type: node_url,
+        setter: function (value) {
+            var done = this.async();
+
+            // if the parameter is not string, an error occurs
+            var url = node_url.parse(String(value));
+
+            if ( !url.host ) {
+                done({
+                    code: 'ETYPE',
+                    message: '"' + value + '" is not a valid url.',
+                    data: {
+                        value: value
+                    }
+                });
+            } else {
+                done(null, url.href);
+            }
+        }
+    },
+
+    stream: {
+        type: node_stream,
+        validator: function (value) {
+            return value instanceof node_stream;
+        }
+    },
+
+    path: {
+        type: node_path,
+        setter: function (value, is_default) {
+
+            // we should not convert `undefined` to some path like `/Users/xxx/undefined`
+            if ( is_default && value === undefined ) {
+                return value;
+            } else {
+                return node_path.resolve(value);
+            }
+        }
+    }
+};
 
 // cwd: {
 //     short: 'c',
@@ -57,36 +202,6 @@ function mix (receiver, supplier, override){
 
     return receiver;
 }
-
-// {
-//     String: {
-//         type: String,
-
-//         // function(data, key, value)
-//         validate: validateString
-//     }
-// }
-
-// So that we will not 
-parser.TYPES = mix({}, nopt.typeDefs);
-
-parser.TYPES.html = {
-    type: 'html',
-    validate: function(data, key, value) {
-        data[key] = String(value);
-    }
-};
-
-parser.TYPES.String = {
-    type: String,
-
-    // If is normal string, strip html tags to prevent XSS attack
-    validate: function (data, key, value) {
-        if(value){
-            data[key] = String(value).replace(/<[^>]+>/g, '');
-        }
-    }
-};
 
 
 parser._parse_rules = function(rules) {
@@ -167,63 +282,4 @@ function is_empty_object (object) {
     return !Object.keys(object).length;
 }
 
-
-var logger = {
-    warn: function (data) {
-        this.warnings.push(data);
-    },
-
-    error: function (data) {
-        this.errors.push(data);
-    },
-
-    info: function (data) {
-        this.infos.push(data);
-    }
-};
-
-
-Object.defineProperties(logger, {
-    _reset: {
-        value: function () {
-            this.warnings = [];
-            this.errors = [];
-            this.infos = [];
-        }
-    },
-
-    warnings: {
-        writable: true
-    },
-
-    errors: {
-        writable: true
-    },
-
-    infos: {
-        writable: true
-    },
-
-    _err: {
-        value: function () {
-            return !!this.errors.length;
-        }  
-    },
-
-    _get: {
-        value: function (key, host) {
-            this._(key, 'warnings', host);
-            this._(key, 'errors', host);
-            this._(key, 'infos', host);
-        }
-    },
-
-    _: {
-        value: function (key, type, host) {
-            if(this[type].length){
-                host[type][key] = this[type];
-            }
-        }
-    }
-});
 
