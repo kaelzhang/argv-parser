a81da401e7ded3e5dd71bd70aec3c186c7d21a3c
kaelzhang <i@kael.me>
2014-08-12 17:17:44 +0800

    migrate old lines; #15

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ module.exports = clean;
 clean.Clean = Clean;
 
 var minimist = require('minimist');
-var skema = require('skema');
-var util = checker.util;
-
-var node_path = require('path');
-var node_url = require('url');
-var node_stream = require('stream');
+var cleaner = require('./lib/cleaner');
 
 
 function clean(options) {
@@ -22,13 +17,9 @@ function clean(options) {
 // - offset: {Number} the offset of the argv at which we should begin to parse
 // - schema: {Object}
 // - shorthands: {Object}
-// checker options:
 // - context: {Object} the context of the helper functions
-// - default_message: {string}
 // - parallel: {boolean=false} whether checker should check the properties in parallel, default to false
 // - limit: {boolean=false} limit to the schema
-// - check_all: {boolean=false} by default, checker will exit immediately when the first error is encountered.
-// - input_mode: {boolean=false}
 function Clean(options) {
   this.options = options = options || {};
 
@@ -38,10 +29,19 @@ function Clean(options) {
 
   this._types = {};
 
-  this._parseSchema();
   this._reverseShorthands();
-  this.checker = checker(this._schema, this.options);
 }
+
+
+// @returns {object|undefined}
+Clean.prototype._get_cleaner = function(key, context) {
+  var rule = this.options.schema && this.options.schema[key];
+  if (!rule) {
+    return;
+  }
+
+  return new Cleaner(rule, context, key);
+};
 
 
 Clean.prototype._reverseShorthands = function() {
@@ -98,7 +98,6 @@ Clean.prototype._parseArgvOptions = function() {
 
 
 Clean.prototype.clean = function(object, callback) {
-  this.checker.check(object, callback);
 };
 
 
@@ -152,222 +151,3 @@ Clean.prototype._argvExists = function(key, argv) {
       || arg === negative_key;
   });
 };
-
-
-// schema
-// <key>: {
-
-//     // schema for argv >>>>>>>>>>>>>>>>
-//     short: 'c',
-//     // '-c' is equivalent to '--cwd <default-short-value>' 
-//     short_pattern: ['--cwd', '<default-short-value>'],
-
-//     // schema for santitizing >>>>>>>>>>>>>>>>>>
-//     default: process.cwd(),
-//     type: node_path,
-//     validator: {function()
-//     setter: {function()}
-//     required: {boolean}
-// }
-Clean.prototype._parseSchema = function() {
-  var schema = checker.parseSchema(this.options.schema);
-  util.map(schema, function(rule, name) {
-    if (rule.required) {
-      rule.validator.unshift(required_validator);
-    }
-
-    var type = rule.type;
-    var type_def = this._getTypeDef(type);
-
-    if (type_def.validator) {
-      rule.validator.unshift(type_def.validator);
-    }
-
-    if (type_def.setter) {
-      rule.setter.unshift(type_def.setter);
-    }
-
-  }, this);
-
-  this._schema = schema;
-};
-
-
-Clean.prototype._getTypeDef = function(type) {
-  var rule = this._types[type] || TYPES[type];
-
-  // must be an object, or abandon it
-  if (Object(rule) === rule) {
-    return rule;
-  }
-
-  var key;
-  var def;
-
-  for (key in TYPES) {
-    def = TYPES[key];
-
-    if (type === def.type) {
-      return def;
-    }
-  }
-
-  return {};
-};
-
-
-function required_validator(value, is_default) {
-  var done = this.async();
-
-  if (is_default) {
-    done({
-      code: 'EREQUIRED',
-      data: {
-        value: value
-      }
-    });
-  } else {
-    done(null);
-  }
-};
-
-
-var TYPES = {
-  string: {
-    type: String,
-    setter: function(value, is_default) {
-      if (is_default && value === undefined) {
-        return '';
-      }
-
-      // If is normal string, strip html tags to prevent XSS attack
-      return String(value).replace(/<[^>]+>/g, '');
-    }
-  },
-
-  number: {
-    type: Number,
-    validator: function(value) {
-      return !isNaN(value);
-    },
-    setter: function(value) {
-      return Number(value);
-    }
-  },
-
-  boolean: {
-    type: Boolean,
-    setter: function(value) {
-      if (value instanceof Boolean) {
-        return value.valueOf();
-
-      } else if (typeof value === 'string') {
-        if (!isNaN(value)) {
-          return !!(+value);
-
-        } else if (value === 'null' || value === 'false') {
-          return false;
-
-        } else {
-          return true;
-        }
-
-      } else {
-        return !!value;
-      }
-    }
-  },
-
-  date: {
-    type: Date,
-    setter: function(value) {
-      var done = this.async();
-
-      var date = Date.parse(value);
-
-      if (isNaN(date)) {
-        done({
-          code: 'ETYPE',
-          message: '"' + value + '" is not a valid date.',
-          data: {
-            value: value
-          }
-        });
-      } else {
-        done(null, new Date(value));
-      }
-    }
-  },
-
-  html: {
-    type: 'html',
-    setter: function(value) {
-      return String(value);
-    }
-  },
-
-  url: {
-    type: node_url,
-    setter: function(value, is_default) {
-      var done = this.async();
-
-      if (is_default && value === undefined) {
-        return done(null, value);
-      }
-
-      // if the parameter is not string, an error occurs
-      var url = node_url.parse(String(value));
-
-      if (!url.host) {
-        done({
-          code: 'ETYPE',
-          message: '"' + value + '" is not a valid url.',
-          data: {
-            value: value,
-            expect: 'url'
-          }
-        });
-
-      } else {
-        done(null, url.href);
-      }
-    }
-  },
-
-  stream: {
-    type: node_stream,
-    validator: function(value) {
-      return value instanceof node_stream;
-    }
-  },
-
-  path: {
-    type: node_path,
-    validator: function(value, is_default) {
-      var done = this.async();
-      if (!is_default && typeof value !== 'string') {
-        return done({
-          code: 'ETYPE',
-          message: '`' + value + '` is not a valid path.',
-          data: {
-            value: value,
-            expect: 'path'
-          }
-        });
-      } else {
-        done(null);
-      }
-    },
-
-    setter: function(value, is_default) {
-      // we should not convert `undefined` to some path like `/Users/xxx/undefined`
-      if (is_default && value === undefined) {
-        return value;
-      } else {
-        return node_path.resolve(String(value));
-      }
-    }
-  }
-};
-
-clean.TYPES = TYPES;
